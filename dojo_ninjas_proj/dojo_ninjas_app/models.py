768001b7e99f15dcf537d7014a20486507b56351
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-04 13:58:01 -0800

    Setup basic layout of database and added 2 modules with 3 to 4 attributes each with one being a ForeignKey and stored a few objects

--- dojo_ninjas_proj/dojo_ninjas_app/models.py
+++ dojo_ninjas_proj/dojo_ninjas_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Dojo(models.Model):
+    loc_name = models.CharField(max_length = 40)
+    city = models.CharField(max_length = 30)
+    state = models.CharField(max_length = 30)
+    desc = models.TextField(null=True)
+    # ninjas (related_name)
+
+class Ninja(models.Model):
+    dojo = models.ForeignKey(Dojo, related_name="ninjas", on_delete = models.CASCADE)
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
